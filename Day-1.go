aed8dcb7ab6edfee9448bbf01305249e6ad325f8
sshiv5768 <sshiv9601@gmail.com>
2022-06-21 20:45:31 +0530

    Day-1 Go Hello World

---
+++ Day-1.go
@@ -0,0 +1,8 @@
+// Day-1
+package main // using main function to run the program 
+
+import "fmt" // importing the fmt package to print the message
+
+func main(){
+	fmt.Println("Hello, Shivlal!") // printing the message
+}
