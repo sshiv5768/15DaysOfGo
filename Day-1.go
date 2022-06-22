43471abc45677832ef8788e425c5f3039b137a98
sshiv5768 <sshiv9601@gmail.com>
2022-06-22 23:47:33 +0530

    Day-1 Hello World and Comments

--- Day-1.go
+++ Day-1.go
@@ -1,4 +1,5 @@
 // Day-1
+// This is a comment. You won't find me in the output.
 package main // using main function to run the program 
 
 import "fmt" // importing the fmt package to print the message
