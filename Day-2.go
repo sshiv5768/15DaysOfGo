682a1df3419c1f5061cdb1306bc089d531148ff4
sshiv5768 <sshiv9601@gmail.com>
2022-06-23 00:01:01 +0530

    Day-2 Variables

---
+++ Day-2.go
@@ -0,0 +1,37 @@
+// Day-2
+package main
+import "fmt"
+func main() {
+
+
+// Variables
+/*
+	Variable Declaration
+	var(keyword) variableName type = value;
+
+	or
+	variableName := value
+*/
+
+// Four main types of variables in Go
+
+// int - for integers
+
+var num1 int = 23;
+
+// float32 - floating point numbers with decimals
+var num2 float32 = 32.32
+
+// string - text such as "Hello World"
+var greetings string = "Good Morning!"
+
+// bool - two state value - true and false
+var reply bool = true
+
+fmt.Println(num1)
+fmt.Println(num2)
+fmt.Println(greetings)
+fmt.Println(reply)
+
+
+}
