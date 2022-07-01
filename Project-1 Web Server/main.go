d5ff6be0eefcbffa00a942bf40846fb61641a2f9
sshiv5768 <sshivlal9601@gmail.com>
2022-07-01 23:58:36 +0530

    Day-11 Project-1 Web server

--- Project-1 Web Server/main.go
+++ Project-1 Web Server/main.go
@@ -10,4 +10,10 @@ func main(){
 	// Creating a new file server
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+
+	// Creating a listenerAndServe at port 8080
+	fmt.Println("Listening on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
