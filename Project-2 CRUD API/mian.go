caa89b6ffbf30bfeddf45c356edf367ef3535e9f
sshiv5768 <sshivlal9601@gmail.com>
2022-07-05 23:22:08 +0530

    Day-15 Project-2 CRUD API

--- Project-2 CRUD API/mian.go
+++ Project-2 CRUD API/mian.go
@@ -36,14 +36,12 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
 
 */
 func main(){
-	movies := append(movies, 
-		Movie{Id: 1, Title: "The Shawshank Redemption", Year: 1994, Director: "Frank Darabont"}, 
-		Movie{Id: 2, Title: "The Godfather", Year: 1972, Director: "Francis Ford Coppola"}, 
-		Movie{Id: 3, Title: "The Godfather: Part II", Year: 1974, Director: "Francis Ford Coppola"}, 
-		Movie{Id: 4, Title: "The Dark Knight", Year: 2008, Director: "Christopher Nolan"},)
+	movies = append(movies, 
+		Movie{Id: 1, Title: "The Shawshank Redemption", Year: 1994, Director: "Frank Darabont"})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/movies", getMovies).Methods("GET")
-	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	
 
 
 	fmt.Println("Server is starting on port 8081")
